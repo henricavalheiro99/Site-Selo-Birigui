e8a9a540b6c32a7c5933985435b94d73a8ec9e97
Henri <124930679+henricavalheiro99@users.noreply.github.com>
2023-08-30 23:48:49 -0300

    Carrossel teoricamente feito

---
+++ Site-Selo-Birigui/assets/script/javascript.js
@@ -0,0 +1,46 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+let isAnimating = false;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", async (e) => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    const isLeftButton = control.classList.contains("arrow-left");
+
+    if (isLeftButton) {
+      currentItem -= 1;
+      if (currentItem < 0) {
+        currentItem = maxItems - 1;
+      }
+    } else {
+      currentItem += 1;
+      if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
+    }
+
+    try {
+      items[currentItem].classList.add("current-item"); // Adicionamos a classe antes da animação
+
+      await items[currentItem].scrollIntoView({
+        inline: "center",
+        behavior: "smooth",
+        block: "nearest"
+      });
+
+      items.forEach((item, index) => {
+        if (index !== currentItem) {
+          item.classList.remove("current-item");
+        }
+      });
+    } catch (error) {
+      console.error("Error scrolling:", error);
+    }
+
+    isAnimating = false;
+  });
+});
